08f9949f6154742b4de56a6582d1f35977d08574
AntonewtonQ <antonewtonquima@gmail.com>
2024-09-20 00:45:59 +0100

    modificado o footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-white py-4 text-center">
-      <p className="mb-2">Desenvolvido por Antonewton Quima</p>
+      <p className="mb-2">Feito com amor ❤️ </p>
       <p className="text-yellow-500 font-semibold">
         "A mensagem do advento a todo o mundo em minha geração"
       </p>
